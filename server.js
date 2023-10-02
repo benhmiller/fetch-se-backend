67cf028334bf0127ad573a5accb4916743149d74
Benjamin Miller <benhmiller22@gmail.com>
2023-10-01 21:40:46 -0500

    Added comments for clarity and updated logic of '/add' route handler

--- server.js
+++ server.js
@@ -12,7 +12,12 @@ const transactions = [];                       // Array storing entire transacti
 var balance = 0;                               // Tracks the total point balance for the user
 var points_per_payer = {}                      // Dictionary tracking points per payer
 
-// 
+/* Route handler for the HTTP POST method with path '/add'. Handles the addition of a new
+ * transaction, which consists of the payer through which the points are added, the number of
+ * points to be added, and a timestamp for the transaction.
+ * 
+ * Expected request body syntax: { "payer" : "DANNON", "points" : 5000, "timestamp" : "2020-11-02T14:00:00Z" }
+ */
 app.post('/add', (req, res) => {
     const numKeysPassed = Object.keys(req.body).length;
     if(numKeysPassed !== 3) { // Verify correct number of keys
@@ -33,23 +38,32 @@ app.post('/add', (req, res) => {
     }
 
     payer = payer.toUpperCase(); // Convert payer to uppercase for consistency
-    
-    if(payer in points_per_payer) { // Update points_per_payer dictionary (either add to existing points or create new item)
-        points_per_payer[payer] += points;
+
+    // Update points_per_payer dictionary (Ensure no payer will have negative points)
+    if (payer in points_per_payer) { // Add to existing item
+        const newPoints = points_per_payer[payer] + points;
+        if (newPoints < 0) {
+            return res.status(400).json({ error: 'Invalid points. Points per payer cannot be negative.' });
+        }
+        points_per_payer[payer] = newPoints;
     }
-    else {
+    else { // Create new item
+        if (points < 0) {
+            return res.status(400).json({ error: 'Invalid points. Points per payer cannot be negative.' });
+        }
         points_per_payer[payer] = points;
     }
 
-    balance += points; // Update total point balance for user
+    balance += points; // Update total points balance for user
 
     transactions.push({ payer, points, timestamp }); // Add transaction to transaction history
-
-    // Account for negative point transactions
-    if(points < 0) { // Decrease available points for spending
+    
+    // Update available points for spending
+    if(points < 0) { // Account for negative point transactions by decreasing available points for spending
         let previousNode = null;
         let currentNode = spendingList.head;
 
+        // Search for payer transactions with positive point values to detract from, continue until all negative points accounted for
         while (currentNode && points < 0) {
             const { payer: currentPayer, points: currentPoints } = currentNode.transaction;
             if (currentPayer === payer) {
@@ -88,28 +102,32 @@ app.post('/add', (req, res) => {
     res.sendStatus(200);
 });
 
+/* Route handler for the HTTP POST method with path '/spend'. Handles the spending of a particular number of
+ * user points by spending payer points in order of oldest to newest transactions. If the number of points
+ * sought to be spent exceeds the user's balance, an error will be thrown.
+ * 
+ * Expected request body syntax: { "points" : 5000 }
+ */
 app.post('/spend', (req, res) => {
-    // Verify correct number of keys
     const numKeysPassed = Object.keys(req.body).length;
-    if(numKeysPassed !== 1) {
+    if(numKeysPassed !== 1) { // Verify correct number of keys
         return res.status(400).json({ error: `Invalid request body. Expected one key, received ${numKeysPassed}.` });
     }
 
-    // Attempt to extract key and check validity
     const {points} = req.body;
-    if(!points) {
+    if(!points) { // Attempt to extract key and check validity
         return res.status(400).json({ error: 'Invalid request body. Required key is missing.' });
     }
 
-    // Check points value
-    if (!Number.isInteger(points) || (balance - points) < 0) {
+    if (!Number.isInteger(points) || (balance - points) < 0) { // Check points value
         return res.status(400).json({ error: `Invalid points. Points must be an integer less than or equal to the current balance: ${balance}.` });
     }
 
     // Perform spend operation
-    const transactionsUsed = [];
-    let pointsSpent = 0;
-    while(points - pointsSpent > 0) {
+    const transactionsUsed = []; // Tracks payers with points used
+    let pointsSpent = 0;         // Tracks total number of points spent
+    
+    while(points - pointsSpent > 0) { // Continue through spending list until payer points equals points sought to be spent
         const transaction = spendingList.peekHead();
         const { payer, points: transactionPoints } = transaction;
 
@@ -118,7 +136,8 @@ app.post('/spend', (req, res) => {
             transactionsUsed.push({ payer, points: -transactionPoints });
             points_per_payer[payer] -= transactionPoints;
             pointsSpent += transactionPoints;
-        } else { // Partially spend the transaction and update spending list
+        }
+        else { // Partially spend the transaction and update spending list
             transactionsUsed.push({ payer, points: -(points - pointsSpent) });
             points_per_payer[payer] -= points - pointsSpent;
             transaction.points -= points - pointsSpent;
@@ -126,13 +145,16 @@ app.post('/spend', (req, res) => {
         }
     }
 
-    // Update Balance
-    balance -= points;
+    balance -= points; // Update total points balance for user
 
-    console.log('Transactions used:', transactionsUsed);
-    res.sendStatus(200);
+    res.status(200).json(transactionsUsed);
 });
 
+/* Route handler for the HTTP GET method with path '/balance'. Handles the getting of the number of points
+ * a user has from each payer.
+ * 
+ * Expected response body syntax: { "DANNON": 1000, ”UNILEVER” : 0, "MILLER COORS": 5300 }
+ */
 app.get('/balance', (req, res) => {
     res.status(200).json(points_per_payer);
 });
